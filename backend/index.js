4d85c8b87d5ca12963a3b55d3a165b8f30a56005
Arzzz <arzzz.efeza86@gmail.com>
2026-06-05 14:23:43 +0800

    Import DB ke Railway

--- backend/index.js
+++ backend/index.js
@@ -49,6 +49,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "",
   database: process.env.DB_NAME || "db_sim",
+  port: process.env.DB_PORT || 3306  // ← tambahkan ini
 });
 
 db.connect((err) => {
